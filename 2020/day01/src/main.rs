df9681cd68ce72b2f4100b0bafac4c0e948308df
Takahiro Kawashige <lapavoni@gmail.com>
2020-12-02 21:19:34 +0900

    Add Advent of Code 2020 day01 part2

--- 2020/day01/src/main.rs
+++ 2020/day01/src/main.rs
@@ -20,6 +20,31 @@ impl Solution {
         }
         0
     }
+
+    fn solve2(nums: Vec<i32>) -> i32 {
+        let mut nums = nums;
+        nums.sort();
+        for i in 0..nums.len() {
+            if 2020 <= nums[i] {
+                break;
+            }
+            for j in (i + 1)..nums.len() {
+                let tmp_sum = nums[i] + nums[j];
+                if 2020 <= tmp_sum {
+                    break;
+                }
+                for k in (j + 1)..nums.len() {
+                    let sum = tmp_sum + nums[k];
+                    if sum == 2020 {
+                        return nums[i] * nums[j] * nums[k];
+                    } else if 2020 < sum {
+                        break;
+                    }
+                }
+            }
+        }
+        0
+    }
 }
 
 use std::io::Read;
@@ -29,7 +54,8 @@ fn main() {
     let mut iter = buf.split_whitespace();
     let _n: usize = iter.next().unwrap().parse().unwrap();
     let nums = iter.map(|i| i.parse::<i32>().unwrap()).collect::<Vec<_>>();
-    println!("{}", Solution::solve(nums));
+    println!("Part1: {}", Solution::solve(nums.clone()));
+    println!("Part2: {}", Solution::solve2(nums));
 }
 
 #[cfg(test)]
@@ -39,5 +65,6 @@ mod test {
     #[test]
     fn test_day01() {
         assert_eq!(1721 * 299, Solution::solve(vec![1721, 979, 366, 299, 675, 1456]));
+        assert_eq!(979 * 366 * 675, Solution::solve2(vec![1721, 979, 366, 299, 675, 1456]));
     }
 }
