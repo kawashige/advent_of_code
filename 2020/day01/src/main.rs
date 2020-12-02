88ecd215ad79d9eced591d83229145966cd955c9
Takahiro Kawashige <lapavoni@gmail.com>
2020-12-02 21:05:45 +0900

    Add Advent of Code 2020

---
+++ 2020/day01/src/main.rs
@@ -0,0 +1,43 @@
+struct Solution {
+}
+
+impl Solution {
+    fn solve(nums: Vec<i32>) -> i32 {
+        let mut nums = nums;
+        nums.sort();
+        for i in 0..nums.len() {
+            if 2020 <= nums[i] {
+                break;
+            }
+            for j in (i + 1)..nums.len() {
+                let sum = nums[i] + nums[j];
+                if sum == 2020 {
+                    return nums[i] * nums[j];
+                } else if 2020 < sum {
+                    break;
+                }
+            }
+        }
+        0
+    }
+}
+
+use std::io::Read;
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+    let mut iter = buf.split_whitespace();
+    let _n: usize = iter.next().unwrap().parse().unwrap();
+    let nums = iter.map(|i| i.parse::<i32>().unwrap()).collect::<Vec<_>>();
+    println!("{}", Solution::solve(nums));
+}
+
+#[cfg(test)]
+mod test {
+    use super::Solution;
+
+    #[test]
+    fn test_day01() {
+        assert_eq!(1721 * 299, Solution::solve(vec![1721, 979, 366, 299, 675, 1456]));
+    }
+}
